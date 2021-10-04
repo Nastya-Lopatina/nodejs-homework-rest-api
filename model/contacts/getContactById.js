16b09886976c329677a2764c8053d8ff715750c3
Nastya-Lopatina <73726295+Nastya-Lopatina@users.noreply.github.com>
2021-10-04 10:37:37 +0900

    Создана ветка hw03

---
+++ model/contacts/getContactById.js
@@ -0,0 +1,11 @@
+
+const { getlistContacts } = require('./getListContacts')
+
+const getContactById = async (contactId) => {
+  const contacts = await getlistContacts()
+  const contact = contacts.find((contact) => contact.id === Number(contactId))
+  console.log(contact)
+  return contact
+}
+
+module.exports = { getContactById }
---
+++ model/contacts/removeContact.js
@@ -0,0 +1,22 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+const contactsPath = path.resolve('../../db/contacts.json')
+const { getContactById } = require('./getContactById')
+const { getListContacts } = require('./getListContacts')
+
+const removeContact = async (contactId) => {
+  const contacts = await getListContacts()
+  const contact = await getContactById(contactId)
+
+  if (!contact) {
+    return undefined
+  }
+  const newContactsList = contacts.filter((item) => item.id !== Number(contactId))
+
+  await fs.writeFile(contactsPath, JSON.stringify(newContactsList), 'utf-8')
+
+  return contact
+}
+
+module.exports = { removeContact }
